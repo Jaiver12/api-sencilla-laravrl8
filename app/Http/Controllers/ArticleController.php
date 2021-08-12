ae93dda6b23a56a19562874a296c79e58a9c97e7
Jaiver Ramos <jaiver.ramos7942@gmail.com>
2021-08-11 19:35:42 -0500

    Api rest sencilla en laravel 8

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Article::all();
+        return $articles;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $article = new Article();
+
+        $article->description = $request->article;
+        $article->price = $request->price;
+        $article->stock = $request->stock;
+
+        $article->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Article $article)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $article = Article::findOrFail($request->id);
+
+        $article->description = $request->article;
+        $article->price = $request->price;
+        $article->stock = $request->stock;
+
+        $article->save();
+
+        return $article;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Article  $article
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+       $article = Article::destroy($request->id);
+       return $article;
+    }
+}
